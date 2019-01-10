a8c67db8930893a41e1d2fabf595d6a37aa39b2f
D. Salinas <dany.asv@gmail.com>
2019-01-09 22:14:34 -0700

    @

--- src/main/java/com/example/service/ClimDataServiceImpl.java
+++ src/main/java/com/example/service/ClimDataServiceImpl.java
@@ -3,7 +3,9 @@ package com.example.service;
 import com.example.domain.ClimData;
 import com.example.persistence.ClimDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ClimDataServiceImpl implements ClimDataService {
     
     private final ClimDataRepository repo;
