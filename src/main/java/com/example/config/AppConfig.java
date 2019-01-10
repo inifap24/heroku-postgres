76ac624a1be125685fad5faf78773cd0ab2d1483
D. Salinas <dany.asv@gmail.com>
2019-01-10 08:49:15 -0700

    @

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -1,18 +1,26 @@
 package com.example.config;
 
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    
+    @Bean(name = "dataSource", destroyMethod = "close")
+    public DataSource masterDataSource(DataConfig dbProperties) {
+        HikariDataSource ds = new HikariDataSource();
+//        ds.setUsername(dbProperties.getUsername());
+//        ds.setPassword(dbProperties.getPassword());
+        ds.setJdbcUrl(dbProperties.getUrl());
+        ds.setDriverClassName(dbProperties.getDriverClassName());
+        ds.setPoolName(dbProperties.getPoolName());
+        ds.setMaximumPoolSize(dbProperties.getMaxPoolSize());
+        ds.setMinimumIdle(dbProperties.getMinIdle());
+        ds.setConnectionTimeout(dbProperties.getConnectionTimeout());
+        ds.setIdleTimeout(dbProperties.getIdleTimeout());
+        return ds;
     }
 }
---
+++ src/main/java/com/example/config/DataConfig.java
@@ -0,0 +1,47 @@
+package com.example.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+@Data
+public class DataConfig {
+    
+    private String url;
+//    private String username;
+//    private String password;
+    private String driverClassName;
+    private long connectionTimeout;
+    private int maxPoolSize;
+    private long idleTimeout;
+    private int minIdle;
+    private String poolName;
+    
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("MasterDatabaseConfigProperties [url=");
+        builder.append(url);
+//        builder.append(", username=");
+//        builder.append(username);
+//        builder.append(", password=");
+//        builder.append(password);
+        builder.append(", driverClassName=");
+        builder.append(driverClassName);
+        builder.append(", connectionTimeout=");
+        builder.append(connectionTimeout);
+        builder.append(", maxPoolSize=");
+        builder.append(maxPoolSize);
+        builder.append(", idleTimeout=");
+        builder.append(idleTimeout);
+        builder.append(", minIdle=");
+        builder.append(minIdle);
+        builder.append(", poolName=");
+        builder.append(poolName);
+        builder.append("]");
+        return builder.toString();
+    }
+    
+}
