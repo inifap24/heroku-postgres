768826014a1eb48ba8e52af16deb9caed0bbd05e
D. Salinas <dany.asv@gmail.com>
2019-01-10 08:59:10 -0700

    @

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,12 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
