5ed08974e3b988cc9afaec962e7bc6e9a2345141
D. Salinas <dany.asv@gmail.com>
2019-01-19 09:32:07 -0700

    @

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -6,7 +6,9 @@ import java.net.URISyntaxException;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class AppConfig {
     
