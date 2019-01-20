12b8c07326466b80b72b16041efeaba03f4b4dfd
D. Salinas <dany.asv@gmail.com>
2019-01-20 11:36:06 -0700

    @

--- src/main/java/tesis/tenant/hibernate/DataSourceBasedMultiTenantConnectionProviderImpl.java
+++ src/main/java/tesis/tenant/hibernate/DataSourceBasedMultiTenantConnectionProviderImpl.java
@@ -1,8 +1,5 @@
 package tesis.tenant.hibernate;
 
-import com.google.common.collect.Iterables;
-import java.util.ArrayList;
-import java.util.List;
 import tesis.tenant.util.DataSourceUtil;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -51,33 +48,3 @@ public class DataSourceBasedMultiTenantConnectionProviderImpl
     }
 
 }
-
-//        datalinks.add("HEROKU_POSTGRESQL_BLACK_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_BLUE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_BRONZE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_BROWN_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_CHARCOAL_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_COBALT_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_COPPER_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_CRIMSON_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_CYAN_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_GOLD_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_GRAY_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_GREEN_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_IVORY_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_JADE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_MAROON_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_MAUVE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_NAVY_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_OLIVE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_ONYX_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_ORANGE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_PINK_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_PUCE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_PURPLE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_RED_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_ROSE_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_SILVER_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_TEAL_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_TEAL_URL");
-//        datalinks.add("HEROKU_POSTGRESQL_YELLOW_URL");
