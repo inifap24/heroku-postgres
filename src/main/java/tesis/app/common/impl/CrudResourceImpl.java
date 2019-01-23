5f4054b7b2046c196a228363f698bc247177c557
D. Salinas <dany.asv@gmail.com>
2019-01-23 01:36:57 -0700

    @

--- src/main/java/tesis/app/common/impl/CrudResourceImpl.java
+++ src/main/java/tesis/app/common/impl/CrudResourceImpl.java
@@ -1,5 +1,6 @@
 package tesis.app.common.impl;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,50 +10,52 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import tesis.app.common.CrudService;
+import tesis.app.common.exception.ResourceNotFoundException;
 
 public abstract class CrudResourceImpl<T, ID> {
 
-    public final CrudService<T, ID> service;
+    public final CrudRepository<T, ID> repo;
 
-    public CrudResourceImpl(CrudService<T, ID> service) {
-        this.service = service;
+    public CrudResourceImpl(CrudRepository<T, ID> repo) {
+        this.repo = repo;
     }
 
     @GetMapping
     public Iterable<T> getAll(@RequestHeader String tenantId) {
-        return service.getAll();
+        return repo.findAll();
     }
 
     @GetMapping("/{id}")
     public T getById(@PathVariable ID id, @RequestHeader String tenantId) {
-        return service.getById(id);
+        return repo.findById(id).orElseThrow(ResourceNotFoundException::new);
     }    
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public T save(@RequestBody T entity, @RequestHeader String tenantId) {
-        return service.save(entity);
+        return repo.save(entity);
     }
 
     @PutMapping("/{id}")
     public T update(@PathVariable ID id, @RequestBody T entity, @RequestHeader String tenantId) {
-        return service.update(id, entity);
+        repo.findById(id).orElseThrow(ResourceNotFoundException::new);      
+        return repo.save(entity);
     }
 
     @DeleteMapping("/{id}")
     public void remove(@PathVariable ID id, @RequestHeader String tenantId) {        
-        service.remove(id);
+        repo.findById(id).orElseThrow(ResourceNotFoundException::new);
+        repo.deleteById(id);
     }
 
     @PostMapping("/saveAll")
     public Iterable<T> saveAll(@RequestBody Iterable<T> entities, @RequestHeader String tenantId) {
-        return service.saveAll(entities);
+        return repo.saveAll(entities);
     }
 
     @DeleteMapping("/removeAll")
     public void removeAll(@RequestBody Iterable<T> entities, @RequestHeader String tenantId) {
-        service.removeAll(entities);
+        repo.deleteAll(entities);
     }
 
 }
