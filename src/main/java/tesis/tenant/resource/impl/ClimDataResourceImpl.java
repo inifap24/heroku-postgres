bed57e3fc87527cf373bcc2544c05f47200a6cc5
dsalinasv <dsalinasv@uas.edu.mx>
2019-04-08 17:33:52 -0600

    Agregue cors

--- src/main/java/tesis/tenant/resource/impl/ClimDataResourceImpl.java
+++ src/main/java/tesis/tenant/resource/impl/ClimDataResourceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -16,6 +17,7 @@ import tesis.tenant.hibernate.DataLinksProperties;
 import tesis.tenant.persistence.ClimDataRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/climDataService")
 public class ClimDataResourceImpl extends CrudResourceImpl<ClimData, String> {
     
